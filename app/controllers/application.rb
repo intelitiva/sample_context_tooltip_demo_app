a0c35292ab552956a01204b93af8ebcc3d71d81b
Vicente Mundim <vicente.mundim@intelitiva.com>
2009-02-20 15:32:04 -0300

    Initial commit

---
+++ app/controllers/application.rb
@@ -0,0 +1,17 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => '86e410e6343a3ce2cd8ca30153089754'
+  
+  # See ActionController::Base for details 
+  # Uncomment this to filter the contents of submitted sensitive data parameters
+  # from your application log (in this case, all fields with names like "password"). 
+  # filter_parameter_logging :password
+
+  layout 'application'
+end
---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,7 @@
+class MainController < ApplicationController
+  def index
+  end
+
+  def prototype
+  end
+end
