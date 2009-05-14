fe94c07b76ab4766f8b72d7e2b9f9eddf30b0915
Vicente Mundim <vicente.mundim@intelitiva.com>
2009-05-14 19:55:47 -0300

    Added some samples of using tooltips for contexts added via ajax

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -8,4 +8,23 @@ class MainController < ApplicationController
   def ajax
     render :partial => 'ajax_content'
   end
+
+  def add_container
+    render_container('added')
+  end
+
+  def add_ajax_container
+    render_container('added_ajax')
+  end
+
+  private
+    def render_container(partial)
+      render :update do |page|
+        if params[:prototype]
+          page.call('insert_html', 'added-containers', render(:partial => partial, :locals => { :index => rand(100000) }))
+        else
+          page.insert_html :bottom, 'added-containers', :partial => partial, :locals => { :index => rand(100000) }
+        end
+      end
+    end
 end
